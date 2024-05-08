0414c3816d4a9f5397694fd87fb186ce5c423c2a
lemony452 <gij9808@gmail.com>
2024-05-08 10:58:07 +0900

    feature: sleep 실행 지연 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// 정해진 시간 ms 동안 실행을 지연하는 함수
+export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
