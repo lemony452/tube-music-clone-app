464cf127755f755aa6f311eac0f1aba135199ad4
lemony452 <gij9808@gmail.com>
2024-05-08 10:59:07 +0900

    feature: 페이지별 error, loading 컴포넌트 적용

---
+++ src/app/channel/[id]/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import ErrorMessage from "@/components/ErrorMessage";
+
+function error() {
+  return <ErrorMessage />;
+}
+
+export default error;
---
+++ src/app/explore/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+import LoadingBar from "@/components/LoadingBar";
+
+function loading() {
+  return <LoadingBar />;
+}
+
+export default loading;
