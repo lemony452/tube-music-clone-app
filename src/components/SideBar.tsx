ec0c20de80ef2a15ef696de7552393f375eef435
lemony452 <gij9808@gmail.com>
2024-05-08 12:02:29 +0900

    feature: SideBar 컴포넌트 추가 - 로고, 네비게이터, 재생목록 세분화한 컴포넌트 필요

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Logo from "./Logo";
+import Navigator from "./Navigator";
+
+function SideBar({ children }: any) {
+  return (
+    <div className="flex h-full">
+      <aside className="w-[240px] border-r-[1px] border-neutral-600">
+        <div className="p-[24px]">
+          <Logo />
+        </div>
+        <div>
+          <Navigator />
+        </div>
+      </aside>
+      <div className="flex-auto">{children}</div>
+    </div>
+  );
+}
+
+export default SideBar;
