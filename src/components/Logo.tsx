bfb1af2d1344148699e9f1145bdd3fb47e706077
lemony452 <gij9808@gmail.com>
2024-05-08 22:53:20 +0900

    feature: 로고 컴포넌트 생성

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
+import IconButton from "./IconButton";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { SiYoutubemusic } from "react-icons/si";
+import { useRouter } from "next/navigation";
 
 function Logo() {
-  return <div>Logo Img</div>;
+  const { push } = useRouter();
+
+  const onClickLogo = () => push("/");
+
+  return (
+    <section className="flex flex-row items-center gap-2">
+      <IconButton icon={<RxHamburgerMenu size={24} />}></IconButton>
+      <div className="cursor-pointer flex flex-row gap-1" onClick={onClickLogo}>
+        <SiYoutubemusic color="red" size={24}></SiYoutubemusic>
+        <div className="text-xl">Tube Music</div>
+      </div>
+    </section>
+  );
 }
 
 export default Logo;
