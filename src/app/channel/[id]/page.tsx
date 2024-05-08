cab9010d83edc5e351fb1e110c2ac55d3a74b7d6
lemony452 <gij9808@gmail.com>
2024-05-08 10:29:56 +0900

    feature: app 디렉터리 구조 세팅

---
+++ src/app/channel/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page(props: any) {
+  return <div>channel {props.params.id} page</div>;
+}
+
+export default page;
---
+++ src/app/library/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>library page</div>;
+}
+
+export default page;
