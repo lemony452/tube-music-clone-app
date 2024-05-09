46c69e77274e562b88749d580c09ad024fbccd9d
lemony452 <gij9808@gmail.com>
2024-05-09 22:30:44 +0900

    feature: 사이드바 오픈 시 닫기 아이콘 토글되도록 로직 추가

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,16 +4,24 @@ import React from "react";
 import IconButton from "./IconButton";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { SiYoutubemusic } from "react-icons/si";
+import { RiCloseFill } from "react-icons/ri";
 import { useRouter } from "next/navigation";
 
-function Logo() {
+function Logo({ isDrawerOpen = false, onClickClose = () => {} }) {
   const { push } = useRouter();
 
   const onClickLogo = () => push("/");
 
   return (
     <section className="flex flex-row items-center gap-2">
-      <IconButton icon={<RxHamburgerMenu size={24} />}></IconButton>
+      {isDrawerOpen ? (
+        <IconButton
+          icon={<RiCloseFill size={24} />}
+          onClickIcon={onClickClose}
+        ></IconButton>
+      ) : (
+        <IconButton icon={<RxHamburgerMenu size={24} />}></IconButton>
+      )}
       <div className="cursor-pointer flex flex-row gap-1" onClick={onClickLogo}>
         <SiYoutubemusic color="red" size={24}></SiYoutubemusic>
         <div className="text-xl">Tube Music</div>
