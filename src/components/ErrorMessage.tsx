6a2e16d2c70dc095db34e79ca60c52aa7c4dbc2e
lemony452 <gij9808@gmail.com>
2024-05-09 12:49:13 +0900

    fix: GridLoader className -> color 옵션으로 수정

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -6,7 +6,7 @@ import { GridLoader } from "react-spinners";
 function ErrorMessage() {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
-      <GridLoader className="red"></GridLoader>
+      <GridLoader color="red"></GridLoader>
       <div>잠시 후 다시 확인해주세요...</div>
     </div>
   );
