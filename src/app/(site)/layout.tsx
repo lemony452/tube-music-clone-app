a1b0218e248139861f5400895bc4fa1367e8ad55
lemony452 <gij9808@gmail.com>
2024-05-09 12:48:40 +0900

    feature: Header 컴포넌트 추가

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
 import React from "react";
 
-function layout({ children }: any) {
-  return <div>{children}</div>;
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full h-full">
+      <Header>{children}</Header>
+    </div>
+  );
 }
 
 export default layout;
