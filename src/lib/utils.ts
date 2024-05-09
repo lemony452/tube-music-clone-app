a4f4049f90830fcc44be63cbfc5ae34fdb3d1aee
lemony452 <gij9808@gmail.com>
2024-05-09 11:39:46 +0900

    feature: 랜덤한 정수를 출력하는 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 // 정해진 시간 ms 동안 실행을 지연하는 함수
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
